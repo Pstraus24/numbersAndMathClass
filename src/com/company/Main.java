77797770f40629a43a580b988c97011c4e7b6f1f
Peter E. Straus <pstraus@students.mchenry.edu>
2018-02-09 21:57:28 -0600

    Added methods for different formatting techniques. Commented out the NumberFormatter for CurrencyFormatter

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,6 +10,8 @@ package com.company;
 import java.text.NumberFormat;
 
 import java.util.Scanner;
+import java.text.DecimalFormat;
+
 
 public class Main {
 
@@ -18,30 +20,63 @@ public class Main {
         // -------------- Declare/initialize variables -----------
         double subTotal = 0;//----------- variable for the sum of amounts entered -------------
         double numberEntered;// ---------- input from user, summed into subTotal ---------
-        double salesTax;// ------ Sales tax 2.5% --------------
-        double suggestedTip;// ------- 17.5% based on subTotal only, not total with taxes -------
         Scanner input = new Scanner(System.in);// -------------Initialize input ---------------
-        do{
+        // -------------- Prompt for values, exit on 0 ----------------------
+        do {
             System.out.print("Enter a price (<0> to end): $ ");
             numberEntered = input.nextDouble();
             subTotal = numberEntered + subTotal;
-            salesTax = subTotal * 0.025;
-            suggestedTip = subTotal * 0.175;
-        }while(numberEntered != 0);
+        } while (numberEntered != 0);
 
-        NumberFormat nf = NumberFormat.getNumberInstance();// -------- formatter for currency display ----
-        nf.setGroupingUsed(true);// ------------- grouping for commas -----------
-        nf.setMinimumFractionDigits(2);
-        nf.setMaximumFractionDigits(2);
-        // ---------- display data --------------
-        System.out.println("\nTotal cost: $ " + nf.format(subTotal));
-        System.out.println("Sales tax of 2.5% on $ " + nf.format(subTotal) + ": $ " + nf.format(salesTax));
-        System.out.println("Suggested tip 17.5% (does not include sales tax): $ " + nf.format(suggestedTip));
-        System.out.println("Amount with taxes: $ " + nf.format(subTotal + salesTax));
-        System.out.println("Note that the taxes are not included in suggested tip");
-        System.out.println("Total amount with suggested tip: $ "
-                + nf.format(subTotal + suggestedTip));
-        // ----totalOut = nf.format(totalAmountDue);
+        usingNumberFormat(subTotal);// ------------ calculate and format using Number Format
+        usingBigDecimal(subTotal);
+
+
+    }
+
+        private static void usingNumberFormat(double userNumber) {
+            double salesTax = userNumber * 0.025;
+            double suggestedTip = userNumber * 0.175;
+
+            //NumberFormat nf = NumberFormat.getNumberInstance();// -------- formatter for currency display ----
+            NumberFormat nf = NumberFormat.getCurrencyInstance();// -------- formatter for currency display ----
+
+
+            nf.setGroupingUsed(true);// ------------- grouping for commas -----------
+            nf.setMinimumFractionDigits(2);
+            nf.setMaximumFractionDigits(2);
+            // ---------- display data --------------
+
+            System.out.println("\nUSING CURRENCY FORMAT");
+            //System.out.println("Total cost: $ " + nf.format(userNumber));
+            System.out.println("Total cost: " + nf.format(userNumber));
+            //System.out.println("Sales tax of 2.5% on $ " + nf.format(userNumber) + ": $ " + nf.format(salesTax));
+            System.out.println("Sales tax of 2.5% on " + nf.format(userNumber) + " is " + nf.format(salesTax));
+            //System.out.println("Sales tax of 2.5% on $ " + nf.format(userNumber) + " is " + nf.format(salesTax));
+            System.out.println("Suggested tip 17.5% (does not include sales tax): " + nf.format(suggestedTip));
+            //System.out.println("Suggested tip 17.5% (does not include sales tax): $ " + nf.format(suggestedTip));
+            System.out.println("Amount with taxes: " + nf.format(userNumber + salesTax));
+            //System.out.println("Amount with taxes: $ " + nf.format(userNumber + salesTax));
+            System.out.println("Note that the taxes are not included in suggested tip");
+            System.out.println("Total amount with suggested tip: "
+                    + nf.format(userNumber + suggestedTip + salesTax));
+            //System.out.println("Total amount with suggested tip: $ " + nf.format(userNumber + suggestedTip + salesTax));
+
+    }
+        private static void usingBigDecimal(double userNumber) {
+
+
+            DecimalFormat priceFormatter = new DecimalFormat("$#0.00"); //setting the format
+            String salesTax = priceFormatter.format(userNumber * 0.025); //rounded operation
+            String suggestedTip = priceFormatter.format(userNumber * 0.175);
+            String totalPrice = priceFormatter.format(userNumber + (userNumber * 0.025));
+            String totalWithTips = priceFormatter.format(userNumber + (userNumber * 0.175) + (userNumber * 0.025));
+            System.out.println("\nUSING BigDecimal");
+            System.out.println("Total cost: " + userNumber);
+            System.out.println("Sales Tax of 2.5% on " + userNumber + " is " + salesTax);
+            System.out.println("Suggested tip 17.5% (does not include sales tax): " + suggestedTip);
+            System.out.println("Total with taxes: " + totalPrice);
+            System.out.println("Total with tax and tip: " + totalWithTips);
 
 
     }
